066b6d3260bef6925e2060869e9ee30123830c6f
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-02-10 07:17:52 +0500

    Fix const correctness issues in Example06.cpp
    Updated member functions to adhere to const correctness rules. Removed errors related to modifying const objects and corrected syntax issues.

--- OOP_Lab04/Example06.cpp
+++ OOP_Lab04/Example06.cpp
@@ -9,23 +9,43 @@ public:
         a = 8;
     }
 
-    int nonconstFucntion( ){
+    // Made const so it can be called by a const object
+    int nonconstFucntion( ) const{
         cout<<"Non Constant Function is called "<<endl;
-        //a=a+10;
         return a;
     }
 
+    // Const function cannot modify data members
     int constFucntion(int a) const{
-        this->a = a + 10; //error
-        return a;
+        return a + 10;
     }
 };
 
 int main( ){
     const test t;
+
     cout<<"Constant Function is called"<<endl;
-    t.a = 10; // error, can't modify const objects
-    cout<<t.nonconstFucntion()://error, can't call non const objects
-    cout<<t.constFucntion(10);
+    cout << t.nonconstFucntion() << endl;
+    cout << t.constFucntion(10);
+
     return 0;
 }
+
+/*
+EXPLANATION:
+
+1) A const member function cannot modify any data member of the class.
+   The statement "this->a = a + 10" caused an error and was removed.
+
+2) A const object cannot modify its data members.
+   The statement "t.a = 10" was removed.
+
+3) A const object can call only const member functions.
+   nonconstFucntion() was marked as const to fix this issue.
+
+4) A syntax error was caused by using ':' instead of ';' after a function call.
+   It was corrected.
+
+RESULT:
+The program now follows proper const correctness rules and compiles successfully.
+*/
