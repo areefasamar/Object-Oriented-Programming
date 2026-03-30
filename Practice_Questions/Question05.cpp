2c9d7919865d58b9f919720297333d29e7ddc499
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-03-31 00:51:01 +0500

    Implement Employee class with display method

---
+++ Practice_Questions/Question05.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+private:
+    const int EmployeeId;
+    static int totalEmployees;
+    static const int MAX_EMPLOYEES = 500;
+
+public:
+    Employee(int id) : EmployeeId(id) {
+        totalEmployees++;
+    }
+
+    void display() const {
+        cout << "ID: " << EmployeeId << endl;
+        cout << "Total Employees so far: " << totalEmployees << endl;
+        cout << "Max Capacity: " << MAX_EMPLOYEES << endl;
+        cout << "----------------------" << endl;
+    }
+};
+
+int Employee::totalEmployees = 0;
+
+int main() {
+    Employee e1(101);
+    e1.display();
+
+    Employee e2(102);
+    e2.display();
+
+    return 0;
+}
